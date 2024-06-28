1f143583bd3748a5e3454fe371df8f6471f6169e
zindel <alexei.golovko@gmail.com>
2024-06-28 14:38:54 +0200

    dictionaries

---
+++ _bin/encode_s.py
@@ -0,0 +1,10 @@
+
+enc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+dec = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+forward = {}
+backward = {}
+for c1, c2 in zip(enc, dec):
+    forward[c1] = c2
+    backward[c2] = c1
+print(forward, backward)
