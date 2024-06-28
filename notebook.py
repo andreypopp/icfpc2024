973c9cfd6f59b5a2adf83d5bd660509b327d26cf
Andrey Popp <8mayday@gmail.com>
2024-06-28 17:40:58 +0400

    Update

--- notebook.py
+++ notebook.py
@@ -1,6 +1,5 @@
-
 enc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
-dec = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+dec = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ "
 
 forward = {}
 backward = {}
@@ -28,4 +27,146 @@ def req(command):
 
 ###
 
-req('get index')
+from collections import deque
+
+def solve_lambda_man(grid):
+    # Find Lambda-Man's starting position
+    start = None
+    pills = set()
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 'L':
+                start = (i, j)
+            elif cell == '.':
+                pills.add((i, j))
+
+    # Define possible moves
+    moves = [
+        ('U', -1, 0),
+        ('R', 0, 1),
+        ('D', 1, 0),
+        ('L', 0, -1)
+    ]
+
+    # BFS
+    queue = deque([(start, '', set())])
+    visited = set()
+
+    while queue:
+        (i, j), path, collected = queue.popleft()
+        
+        if (i, j) in pills:
+            collected = collected | {(i, j)}
+        
+        if len(collected) == len(pills):
+            return path
+
+        if (i, j, tuple(sorted(collected))) in visited:
+            continue
+        
+        visited.add((i, j, tuple(sorted(collected))))
+
+        for direction, di, dj in moves:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != '#':
+                queue.append(((ni, nj), path + direction, collected))
+
+    return None  # No solution found
+
+# Example usage
+grid = [
+"#####################",
+"#...#.#.........#...#",
+"#.###.#.#####.###.###",
+"#...#.#.....#.......#",
+"###.#.#.###.#########",
+"#.#....L..#.#.......#",
+"#.#####.###.#.###.###",
+"#.#.#...#.......#...#",
+"#.#.#######.#######.#",
+"#.#...#.#...#.#.....#",
+"#.#.###.#.###.###.#.#",
+"#.....#...#.......#.#",
+"#.###.###.###.#####.#",
+"#.#.#...#...#...#...#",
+"###.#.#.#.#####.###.#",
+"#...#.#...#.....#...#",
+"#.###.#.#.#####.#####",
+"#.....#.#.....#.#...#",
+"#.###.#.#.#.#.#.#.###",
+"#.#...#.#.#.#.#.....#",
+"#####################",
+]
+
+solution = solve_lambda_man(grid)
+print(solution) # UDLLLDURRRRRURR
+
+###
+
+from heapq import heappush, heappop
+
+def manhattan_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def solve_lambda_man2(grid):
+    # Find Lambda-Man's starting position and pills
+    start = None
+    pills = set()
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 'L':
+                start = (i, j)
+            elif cell == '.':
+                pills.add((i, j))
+
+    # Define possible moves
+    moves = [('U', -1, 0), ('R', 0, 1), ('D', 1, 0), ('L', 0, -1)]
+
+    # A* search
+    heap = [(0, 0, start, '', frozenset())]
+    visited = set()
+
+    while heap:
+        _, cost, (i, j), path, collected = heappop(heap)
+        
+        if (i, j) in pills:
+            collected = collected | {(i, j)}
+        
+        if len(collected) == len(pills):
+            return path
+
+        state = (i, j, collected)
+        if state in visited:
+            continue
+        
+        visited.add(state)
+
+        for direction, di, dj in moves:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != '#':
+                new_collected = collected | ({(ni, nj)} if (ni, nj) in pills else set())
+                new_cost = cost + 1
+                print(pills - new_collected)
+                heuristic = min(manhattan_distance((ni, nj), p) for p in (pills - new_collected))
+                heappush(heap, (new_cost + heuristic, new_cost, (ni, nj), path + direction, new_collected))
+
+    return None  # No solution found
+
+# Example usage
+grid = [
+    "###.#...",
+    "...L..##",
+    ".#######"
+]
+
+solution = solve_lambda_man2(grid)
+print(solution)
+
+###
+
+print(req('get scoreboard'))
+
+###
+
+
+print(req('solve lambdaman3 DRDRLLULDLLUUURRLDUULUURRRRRDDULLLRDRDRD'))
